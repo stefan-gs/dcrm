39cb21f34c0eb6ca3b855e664ed74cb310444392
Stefan H <gshaineala@gmail.com>
2023-11-02 08:12:56 +0200

    Initial

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = 'Password123'
+)
+
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute('CREATE DATABASE stefan')
+
+print('All Done!')
